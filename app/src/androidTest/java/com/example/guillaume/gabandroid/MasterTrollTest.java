7eae3b6d559cfd0e5f537d3e7b73e8b6407692ce
SPONY Guillaume <sponyguillaume@gmail.com>
2017-04-21 08:33:12 +0200

    demo pics + tests

---
+++ app/src/androidTest/java/com/example/guillaume/gabandroid/MasterTrollTest.java
@@ -0,0 +1,72 @@
+package com.example.guillaume.gabandroid;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
+import static android.support.test.espresso.Espresso.onView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MasterTrollTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
+
+    @Test
+    public void trollTestMethod() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.guillaume.gabandroid", appContext.getPackageName());
+
+        reportHelper.label("No Troll");
+
+        onView(withId(R.id.revealBtn)).perform(click());
+        reportHelper.label("Troll1");
+
+        onView(withId(R.id.revealBtn)).perform(click());
+        reportHelper.label("Troll2");
+
+        onView(withId(R.id.revealBtn)).perform(click());
+        reportHelper.label("Troll3");
+
+
+
+    }
+}
--- app/src/main/java/com/example/guillaume/gabandroid/MainActivity.java
+++ app/src/main/java/com/example/guillaume/gabandroid/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.guillaume.gabandroid;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +15,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 import com.microsoft.azure.mobile.MobileCenter;
 import com.microsoft.azure.mobile.analytics.Analytics;
 import com.microsoft.azure.mobile.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView _imageView;
+    private int _trollId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +35,16 @@ public class MainActivity extends AppCompatActivity {
         MobileCenter.start(getApplication(), "eb76f60e-ed9d-4057-bb2d-174d04f75aaf", Analytics.class, Crashes.class);
 
 
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        _imageView = (ImageView) findViewById(R.id.image);
+
+
+
+
     }
 
     @Override
@@ -55,5 +66,52 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btnCrash(View view) throws Exception {
+        throw new Exception("Tiens donc un crash");
+    }
+
+
+    public void btnChngPics(View view) {
+
+        changePictureImage();
+        Snackbar.make(view, "Coucou", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+
+    }
+
+
+    private void changePictureImage()
+    {
+        switch (_trollId)
+        {
+            case 0:
+                setImage(R.drawable.amc01);
+                break;
+            case 1:
+                setImage(R.drawable.amc02);
+                break;
+            case 2:
+                setImage(R.drawable.amc03);
+                break;
+            case 3:
+                setImage(R.drawable.amc01);
+                _trollId = 0;
+                break;
+
+
+        }
+        _trollId ++;
+
+    }
+
+    private void setImage(final int drawable)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            _imageView.setImageDrawable(getResources().getDrawable(drawable, getApplicationContext().getTheme()));
+        } else {
+            _imageView.setImageDrawable(getResources().getDrawable(drawable));
+        }
     }
 }
